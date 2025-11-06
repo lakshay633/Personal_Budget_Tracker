e0e674ce59d494fb1dd354089774e63c2f9a6d24
Lakshay Agarwal <633lakshay@gmail.com>
2025-11-06 21:13:20 +0530

    adding update and delete in budget

--- budget-tracker-backend/budgets/urls.py
+++ budget-tracker-backend/budgets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import BudgetListView, BudgetCreateView
+from .views import BudgetListView, BudgetCreateView, BudgetRetrieveUpdateDeleteView
 
 urlpatterns = [
     path("", BudgetListView.as_view(), name="budgets"),
     path("create/", BudgetCreateView.as_view(), name="budget-create"),
+    path("<int:pk>/", BudgetRetrieveUpdateDeleteView.as_view(), name="budget-detail"),
 ]
